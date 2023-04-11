390b8cb5c78b4b43ad65a2fe32d68462535fa6d3
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2023-04-11 23:49:23 +0500

    button меняет текст на stop и play. seekBar блокируется когда идет отчёт.

--- app/src/main/java/com/example/cooltimer/MainActivity.java
+++ app/src/main/java/com/example/cooltimer/MainActivity.java
@@ -6,6 +6,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -13,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
 
     private SeekBar seekBar;
     private TextView textView;
+    private boolean isTimerOn; //создаём переменную для таймера
+    private Button button; // чтоб изменять текст на кнопке
+
+    private CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +29,9 @@ public class MainActivity extends AppCompatActivity {
 
         seekBar.setMax(600); // устанавливаем max значение (в нашем случае 10 минут = 600 секунд)
         seekBar.setProgress(30); // устанавливаем таймер на 30 сек.
+        isTimerOn = false; // ставим что изначально не вкл.
 
+        button = findViewById(R.id.button);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { // привязывем TextView и SeedBar
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -45,19 +52,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void start(View view) { // создаём таймер
-        CountDownTimer countDownTimer = new CountDownTimer(seekBar.getProgress() * 1000, 1000) { // передаем значение от ползунка к TextView
-            @Override
-            public void onTick(long millisUntilFinished) { // данный метод обновляет каждую секунду
-                updateTimer(millisUntilFinished);
-            }
 
-            @Override
-            public void onFinish() {
-                MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.bell_sound); // добавляем звук колокольчика при окончании таймера
-                mediaPlayer.start(); // запускает звук при окончании таймера
-            }
-        };
-        countDownTimer.start();
+        if (!isTimerOn) { // делаем проверку таймера. Знак ! говорит о притивоположности. Таймер не вкл.
+            button.setText("Stop"); //изменяем текст кнопки
+            seekBar.setEnabled(false); //откл. возможность двигать ползунок
+            isTimerOn = true;
+
+            // перенесли код запуска таймера. Запускаем таймер если он не вкл.
+            countDownTimer = new CountDownTimer(seekBar.getProgress() * 1000, 1000) { // передаем значение от ползунка к TextView
+                @Override
+                public void onTick(long millisUntilFinished) { // данный метод обновляет каждую секунду
+                    updateTimer(millisUntilFinished);
+                }
+
+                @Override
+                public void onFinish() {
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.bell_sound); // добавляем звук колокольчика при окончании таймера
+                    mediaPlayer.start(); // запускает звук при окончании таймера
+                }
+            };
+            countDownTimer.start();
+        } else {
+            countDownTimer.cancel(); // остановить переменную
+            textView.setText("00:30"); //возвращаем цифры по умолчанию
+            button.setText("Start"); //возвращаем текст по умолчанию
+            seekBar.setEnabled(true); //вкл. возможность двигать ползунок
+            seekBar.setProgress(30);
+            isTimerOn = false;
+        }
     }
 
     private void updateTimer(long millisUntilFinished) { // создали переменную для рефакторинга кода
