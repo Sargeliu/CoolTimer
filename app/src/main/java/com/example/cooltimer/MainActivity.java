9b7a03f3be6945039ece7bf4e170d4161fd75fed
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2023-04-13 00:18:04 +0500

    Связываем checkboxPreferences c SharedPrefernces. Мы сможем вкл. или откл. звук колокольчика.

--- app/src/main/java/com/example/cooltimer/MainActivity.java
+++ app/src/main/java/com/example/cooltimer/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.cooltimer;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -72,8 +74,12 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onFinish() {
-                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.bell_sound); // добавляем звук колокольчика при окончании таймера
-                    mediaPlayer.start(); // запускает звук при окончании таймера
+
+                    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                    if (sharedPreferences.getBoolean("enable_sound", true)) {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.bell_sound); // добавляем звук колокольчика при окончании таймера
+                        mediaPlayer.start(); // запускает звук при окончании таймера
+                    }
                     resetTimer(); // по окончанию вернёт значения по умолчанию
                 }
             };
