af8fe22d8e10ccbfb6861d43729be2b418b8d847
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2023-04-12 16:15:14 +0500

    В меню настройки(Settings) добавили checkbox вкл. откл. звука. SettingsFragment.

---
+++ app/src/main/java/com/example/cooltimer/SettingsFragment.java
@@ -0,0 +1,11 @@
+package com.example.cooltimer;
+
+import android.os.Bundle;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        addPreferencesFromResource(R.xml.timer_preferences);
+    }
+}
